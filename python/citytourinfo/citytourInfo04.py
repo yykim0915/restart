773bb9fea7876ac3b32ffa561b886af033ee67c7
yykim0915 <107624578+yykim0915@users.noreply.github.com>
2022-07-21 12:05:13 +0900

    upload

--- python/citytourinfo/citytourInfo04.py
+++ python/citytourinfo/citytourInfo04.py
@@ -1,11 +1,10 @@
 import pandas as pd
+from pymongo import MongoClient
 
 #python - mongDB Compass 연결하기
 #pip install pymongo
-from pymongo import MongoClient
-
 client = MongoClient("mongodb://3.38.64.25:27017")
-db = client['test'] #DBname(test)에 접속
+db = client['test'] #DBname-test에 접속
 #print(client.list_database_names())
 #['READ__ME_TO_RECOVER_YOUR_DATA', 'admin', 'config', 'test']
 
@@ -20,6 +19,7 @@ db = client['test'] #DBname(test)에 접속
 #     'latitude' : '37.8245457',
 #     'longitude' : '127.5154443'
 # }
+# dpInsert = db.citytourinfo.insert_one(data)
 #     {
 #     'SIGUN_CD' : '경기도',
 #     'SIGUN_NM' : '가평군',
@@ -29,19 +29,18 @@ db = client['test'] #DBname(test)에 접속
 #     'latitude' : '37.8293425',
 #     'longitude' : '127.5154088'
 # }
-# dpInsert = db.citytourinfo.insert_one(data)
+
 
 
 #test(db)-citytourinfo(collection) 데이터 출력하기
 for d in db['citytourinfo'].find():
     print(d['SIGUN_CD'], d['SIGUN_NM'], d['CITYTOUR_COURSE'], d['CITYTOUR_COURSE_INFO'], d['addr'], d['latitude'], d['longitude'])
-#print(db.citytourinfo.find_one({'CITYTOUR_COURSE':'가평시티투어'})['text']) #1행 출력
+    #print(db.citytourinfo.find_one({'CITYTOUR_COURSE':'가평시티투어'})['text']) #1행 출력
 
 #map에 필요한 column만 저장
-df_mapsample = df[['CITYTOUR_COURSE','CITYTOUR_COURSE_INFO','latitude','longitude']]
-print(df_mapsample)
+# df_mapsample = df[['CITYTOUR_COURSE','CITYTOUR_COURSE_INFO','latitude','longitude']]
+# print(df_mapsample)
 
 #map에 필요한 column만 저장
-# df=pd.DataFrame([SIGUN_CD,SIGUN_NM,CITYTOUR_COURSE,CITYTOUR_COURSE_INFO,addr,latitude,longitude])
-# df.columns=['CITYTOUR_COURSE','CITYTOUR_COURSE_INFO','latitude','longitude']
-# print(df)
+df = pd.DataFrame(data, columns=['CITYTOUR_COURSE_INFO', 'addr']) #시티투어코스정보,주소
+print(df)
